d57ca194060bf8050ba593702aa8f4f2177777bd
HeeJaeMon123 <66413753+HeeJaeMon123@users.noreply.github.com>
2021-08-18 15:54:43 +0900

    Update main.py

--- news topic prediction/main.py
+++ news topic prediction/main.py
@@ -12,3 +12,9 @@ if __name__ == '__main__':
     X_train, X_test, vector = vectorizor(preprocessed_train, preprocessed_test)
     result = train(X_train, y_train, svm.LinearSVC())
     print(test(result, X_test, y_test))
+    
+    test = pd.read_csv('./data/test_data.csv')
+    test_data = preprocessing(test, stopword)
+    res_test = vector.transform(test_data)
+    pred = predict(result, res_test)
+    print(pred)
